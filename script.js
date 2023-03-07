945db279b619a002ac93a91b50fe4a78f74e2a6c
Paul Ivan Dela Cruz <kmpkhai001@gmail.com>
2023-03-07 19:08:27 +0800

    Added new files

---
+++ script.js
@@ -0,0 +1,7 @@
+
+var typed = new Typed(".auto-input", {
+    strings: ["Web Developer", "UI/UX Designer", "Freelancer", "Graphic Designer"],
+    typeSpeed: 100,
+    backSpeed: 70,
+    loop: true
+})
